be386183553cf8bbfc08d4cfc49cfb6b9dc10c0d
collinsdawns <92228998+collinsdawns@users.noreply.github.com>
2023-01-06 08:59:33 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,9 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my Website!" />
+        <Header title="Welcome to FunikNet" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Hello World
         </p>
       </main>
 
